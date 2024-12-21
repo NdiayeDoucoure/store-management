1bea33cf4a747fa3e8912783343974d217a1ee26
NdiayeDoucoure <doucoure.papa10@gmail.com>
2024-12-21 11:21:56 +0000

    feat(services): code refactoring

--- src/main/java/com/taysirsoftware/store/services/CategoryService.java
+++ src/main/java/com/taysirsoftware/store/services/CategoryService.java
@@ -9,27 +9,24 @@ public class CategoryService {
 
     private final EntityManagerFactory emf;
 
-    // Initialization of the EntityManagerFactory with the persistence unit defined in persistence.xml
     public CategoryService(EntityManager em) {
         this.emf = Persistence.createEntityManagerFactory("storePU");
     }
 
-    // Method to add a category
     public void addCategory(Category category) {
         EntityManager em = emf.createEntityManager();
         try {
             em.getTransaction().begin();
-            em.persist(category); // Persist the object in the database
+            em.persist(category);
             em.getTransaction().commit();
         } catch (Exception e) {
             em.getTransaction().rollback();
-            throw e; // Propagate the exception
+            throw e;
         } finally {
             em.close();
         }
     }
 
-    // Method to list all categories
     public List<Category> getAllCategories() {
         EntityManager em = emf.createEntityManager();
         try {
@@ -39,34 +36,32 @@ public class CategoryService {
         }
     }
 
-    // Method to update a category
     public void updateCategory(Category category) {
         EntityManager em = emf.createEntityManager();
         try {
             em.getTransaction().begin();
-            em.merge(category); // Update the object in the database
+            em.merge(category);
             em.getTransaction().commit();
         } catch (Exception e) {
             em.getTransaction().rollback();
-            throw e; // Propagate the exception
+            throw e;
         } finally {
             em.close();
         }
     }
 
-    // Method to delete a category
     public void deleteCategory(Long categoryId) {
         EntityManager em = emf.createEntityManager();
         try {
             em.getTransaction().begin();
             Category category = em.find(Category.class, categoryId);
             if (category != null) {
-                em.remove(category); // Remove the object from the database
+                em.remove(category);
             }
             em.getTransaction().commit();
         } catch (Exception e) {
             em.getTransaction().rollback();
-            throw e; // Propagate the exception
+            throw e;
         } finally {
             em.close();
         }
